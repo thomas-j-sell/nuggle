3843e2f702eb5db4b9eb7449d3475a13fbb43620
Tom Sell <thomas.j.sell@gmail.com>
2017-03-08 18:59:24 -0500

    add support for command line arguments

--- nuggle.rb
+++ nuggle.rb
@@ -2,8 +2,16 @@ require 'pry'
 require_relative 'board'
 require_relative 'solver'
 
-board = Board.new(3, 3)
+width = ARGV[0] ? ARGV[0].to_i : 3
+height = ARGV[1] ? ARGV[1].to_i : width
 
+if width > 4 or height > 4
+  puts "puzzles with dimensions greater than four can take a long time to process.  Continue [y/n]"
+  x = STDIN.gets.chomp
+  exit unless x == 'y'
+end
+
+board = Board.new(width, height)
 board.print
 
 solver = Solver.new(board)
